a17c22719e32c0179dc50b189ddceb018d27b6aa
Ustenko <free.ukrainev@gmail.com>
2021-02-19 16:29:39 +0200

    19-02-2021 16:29

---
+++ 1.JavaSyntax/src/com/javarush/task/jdk13/task01/task0110/Solution.java
@@ -0,0 +1,11 @@
+package com.javarush.task.jdk13.task01.task0110;
+
+/* 
+Вывод на экран переменной
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+    }
+}
